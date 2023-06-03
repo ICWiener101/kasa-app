27db1cb235b31f6325ff4bde0e1b94fc192e1c05
ICWiener101 <plamen.stoyanov87@gmail.com>
2023-06-04 00:55:44 +0200

    some work on the responsive design

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,7 @@ function Header({ image }) {
       return (
             <div className="header-container">
                   <img src={image} alt="" />
+                  <p className="slogan">Chez vous, partout et ailleurs</p>
             </div>
       );
 }
