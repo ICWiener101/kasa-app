ebafb22aa6a75a23c6a5778ed987f4bd808acd91
ICWiener101 <plamen.stoyanov87@gmail.com>
2023-06-24 15:03:10 +0200

    added loading screen

--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -1,8 +1,23 @@
 import Card from './../Card/index';
 import '../../styles/cardContainer.scss';
 import appartmentList from '../../data/db';
+import { useState, useEffect } from 'react';
 
 function CardContainer() {
+      const [isLoading, setIsLoading] = useState(true);
+
+      useEffect(() => {
+            const timer = setTimeout(() => {
+                  const fetchedData = appartmentList;
+
+                  setIsLoading(false);
+            }, 1000);
+
+            return () => clearTimeout(timer);
+      }, []);
+      if (isLoading) {
+            return <div className="container-loading">Loading...</div>;
+      }
       return (
             <div className="card-container">
                   {appartmentList.map((appartment) => (
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,12 +5,27 @@ import Carousel from './../../components/Carousel/index';
 import Info from './../../components/Info/index';
 import '../../styles/details.scss';
 import Error from './../Error/index';
+import { useState, useEffect } from 'react';
 
 function Details() {
       const { id } = useParams();
       const appartment = appartmentList.find((item) => item.id === id);
       if (!appartment) {
             return <Error />;
+      }
+      const [isLoading, setIsLoading] = useState(true);
+
+      useEffect(() => {
+            const timer = setTimeout(() => {
+                  const fetchedData = appartmentList;
+
+                  setIsLoading(false);
+            }, 1500);
+
+            return () => clearTimeout(timer);
+      }, []);
+      if (isLoading) {
+            return <div className="details-loading">Loading...</div>;
       }
       return (
             <div className="details-wrapper">
